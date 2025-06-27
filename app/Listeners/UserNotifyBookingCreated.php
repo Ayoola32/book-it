f44e3f839ef90674e18e6b3b0a5ff033cc4d23f0
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-06-27 11:45:12 +0100

    feat: add UserNotifyBookingCreated listener and UserNotificationBookingCreated notification for booking notifications

---
+++ app/Listeners/UserNotifyBookingCreated.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\BookingCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Notifications\Notification;
+use App\Notifications\UserNotificationBookingCreated;
+
+class UserNotifyBookingCreated
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(BookingCreated $event): void
+    {
+        $email = $event->appointment->email;
+        \Notification::route('mail',$email)->notify(new UserNotificationBookingCreated($event->appointment));
+    }
+}
