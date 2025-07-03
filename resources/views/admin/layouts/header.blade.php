8fc35e37d9fb7f6673b650a0b8e0e9810cb01f33
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-07-03 15:17:14 +0100

    fix: update user display information and improve profile links in header

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -136,16 +136,14 @@
                     aria-label="Open user menu">
                     <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
                     <div class="d-none d-xl-block ps-2">
-                        <div>Paweł Kuna</div>
-                        <div class="mt-1 small text-secondary">UI Designer</div>
+                        <div>{{ Auth::user()->name}}</div>
+                        <div class="mt-1 small text-secondary">Admin</div>
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <a href="#" class="dropdown-item">Status</a>
-                    <a href="./profile.html" class="dropdown-item">Profile</a>
-                    <a href="#" class="dropdown-item">Feedback</a>
+                    <a href="{{ route('admin.profile')}}" class="dropdown-item">Profile</a>
                     <div class="dropdown-divider"></div>
-                    <a href="./settings.html" class="dropdown-item">Settings</a>
+                    <a href="{{ route('admin.settings.index' )}}" class="dropdown-item">Settings</a>
                     <a href="" onclick="event.preventDefault(); getElementById('logout').submit();"
                         class="dropdown-item">Logout</a>
                     <!-- Authentication -->
